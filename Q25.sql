50e16defcd86b2a4ed35c58d43806c19ecc08c0a
yoookurt <110063320+yoookurt@users.noreply.github.com>
2022-08-05 01:55:24 -0500

    Create Q25.sql
    Signed-off-by: yoookurt <110063320+yoookurt@users.noreply.github.com>

---
+++ Q25.sql
@@ -0,0 +1,20 @@
+/*
+Revisit your answer in (19). Convert the result in JSON string and save it to the server using TSQL FOR JSON PATH.
+*/
+
+USE [WideWorldImporters]
+GO
+
+CREATE TABLE dbo.Archive (
+	ID int identity(1,1) primary key,
+	Content nvarchar(max)
+	)
+GO
+
+DECLARE @x NVARCHAR(MAX) =
+  (SELECT *
+     FROM Sales.vStkGrpSldbyName
+     FOR JSON AUTO, INCLUDE_NULL_VALUES)
+insert into dbo.Archive
+select @x 
+GO
